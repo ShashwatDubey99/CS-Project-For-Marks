2f685e8969423925daa13e11eace3539f4214076
Shashwat Dubey <90603919+ShashwatDubey99@users.noreply.github.com>
2025-01-12 15:43:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,84 +1,54 @@
-# main.py
 import utils
 
-def main_menu():
-    print("""
-    1. Add Customer
-    2. View Customers
-    3. Add Vehicle
-    4. View Vehicles
-    5. Add Service Request
-    6. View Services
-    7. Exit
-    """)
-
 def main():
-    # Connect to database
-    connection = utils.connect_to_database(host="localhost", user="root", password="chill@hell", database="project")
-
-    if not connection:
-        return
-
-    # Create tables if they don't exist
-    utils.create_tables(connection)
+    # Initialize the database
+    utils.initialize_db()
 
     while True:
-        main_menu()
-        choice = input("Enter your choice: ")
+        print("\n--- Vehicle Service Center Management ---")
+        print("1. Add Vehicle Type")
+        print("2. Remove Vehicle Type")
+        print("3. Add Customer")
+        print("4. Remove Customer")
+        print("5. Add Service")
+        print("6. Remove Service")
+        print("7. Register Vehicle")
+        print("8. Update Service Status")
+        print("9. View Details by Status")
+        print("10. View All Tables (Vehicle Types & Customers)")
+        print("11. Generate Bill")
+        print("12. Exit")
 
-        if choice == '1':
-            name = input("Enter Name: ")
-            contact = input("Enter Contact: ")
-            email = input("Enter Email: ")
-            address = input("Enter Address: ")
-            utils.add_customer(connection, name, contact, email, address)
+        choice = input("Enter your choice (1-12): ")
 
-        elif choice == '2':
-            customers = utils.view_customers(connection)
-            if customers:
-                for customer in customers:
-                    print(customer)
-            else:
-                print("No customers found.")
-
-        elif choice == '3':
-            make = input("Enter Vehicle Make: ")
-            model = input("Enter Vehicle Model: ")
-            registration_number = input("Enter Registration Number: ")
-            owner_id = input("Enter Owner ID: ")
-            utils.add_vehicle(connection, make, model, registration_number, owner_id)
-
-        elif choice == '4':
-            vehicles = utils.view_vehicles(connection)
-            if vehicles:
-                for vehicle in vehicles:
-                    print(vehicle)
-            else:
-                print("No vehicles found.")
-
-        elif choice == '5':
-            vehicle_id = input("Enter Vehicle ID: ")
-            service_type = input("Enter Service Type: ")
-            description = input("Enter Description: ")
-            status = input("Enter Status: ")
-            cost = input("Enter Cost: ")
-            date = input("Enter Date (YYYY-MM-DD): ")
-            utils.add_service(connection, vehicle_id, service_type, description, status, cost, date)
-
-        elif choice == '6':
-            services = utils.view_services(connection)
-            if services:
-                for service in services:
-                    print(service)
-            else:
-                print("No services found.")
-
-        elif choice == '7':
-            print("Exiting program. Goodbye!")
+        if choice == "1":
+            utils.add_vehicle_type()
+        elif choice == "2":
+            utils.remove_vehicle_type()
+        elif choice == "3":
+            utils.add_customer()
+        elif choice == "4":
+            utils.remove_customer()
+        elif choice == "5":
+            utils.add_service()
+        elif choice == "6":
+            utils.remove_service()
+        elif choice == "7":
+            utils.register_vehicle()
+        elif choice == "8":
+            utils.update_service_status()
+        elif choice == "9":
+            utils.view_details_by_status()
+        elif choice == "10":
+            utils.display_tables()
+        elif choice == "11":
+            utils.generate_bill()
+        elif choice == "12":
+            print("Exiting... Goodbye!")
             break
-
         else:
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
+
